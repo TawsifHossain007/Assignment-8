346c7e9a751a9cce3078689919ad09dc551880ba
TawsifHossain007 <tawsifhossain18.o@gmail.com>
2025-10-08 01:12:31 +0600

    first commit

---
+++ src/Header/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+import LogoImg from "../assets/logo.png";
+import { Github } from "lucide-react";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <Link to="/">
+        <li className="m-2">Home</li>
+      </Link>
+      <Link to="/about">
+        <li className="m-2">Apps</li>
+      </Link>
+      <Link to="/ReadList">
+        <li className="m-2">Installation</li>
+      </Link>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <div className="flex justify-center item ml-0 md:ml-8">
+          <img className="h-[40px] w-[40px]" src={LogoImg} alt="" />
+          <a className="btn btn-ghost text-xl text-[#632ee3]">HERO.IO</a>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end mr-0 md:mr-8">
+        <a className="btn bg-gradient-to-r from-[#632ee3] to-[#9f62f2] text-white rounded-[4px]"><Github></Github> Contribute</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import errorPageImg from '../../assets/OBJECTS.png'
+
+const ErrorPage = () => {
+    return (
+        <div className='flex flex-col justify-center items-center mt-0 md:mt-16'>
+           <img src={errorPageImg} alt="" />
+           <h1 className='font-semibold text-[48px] mt-10'>Oops, page not found!</h1>
+           <p className='font-normal text-[20px] text-[#627382] mt-2'>The page you are looking for is not available.</p>
+           <button className='text-white bg-gradient-to-r from-[#632ee3] to-[#9f62f2] py-3 px-8 rounded-lg mt-6'>Go Back!</button>
+        </div>
+    );
+};
+
+export default ErrorPage;
